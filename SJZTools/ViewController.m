9344ffe969fbb9bd4dad5e617ed47ae1c2394a44
SJZ <>
2018-10-25 17:28:16 +0800

    123456

--- SJZTools/ViewController.m
+++ SJZTools/ViewController.m
@@ -19,17 +19,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//    [UIButton buttonWithTitle:@"确定" norColor:[UIColor whiteColor] font:[UIFont systemFontOfSize:15] backColor:[UIColor redColor] superView:self.view complete:^(UIButton *btn) {
-//        btn.frame = CGRectMake(100, 200, 100, 100);
-//    } touchUpBlock:^(UIButton *btn) {
-//
-//    }];
-    
-    [UIButton buttonWithTitle:@"确定" norColor:[UIColor whiteColor] selColor:[UIColor redColor] font:[UIFont systemFontOfSize:15] norImage:nil selImage:nil backColor:[UIColor redColor] superView:self.view complete:^(UIButton *btn) {
+    [UIButton buttonWithTitle:@"123" norColor:nil heiColor:nil selColor:nil font:[UIFont systemFontOfSize:12] norImage:nil heiImage:nil selImage:nil backColor:[UIColor redColor] borderWidth:0 borderColor:nil cornerRadius:0 superView:self.view complete:^(UIButton *btn) {
         btn.frame = CGRectMake(100, 200, 100, 100);
     } touchUpBlock:^(UIButton *btn) {
-        
+        NSLog(@"我我我我我哦我");
     }];
+    
 }
 
 
